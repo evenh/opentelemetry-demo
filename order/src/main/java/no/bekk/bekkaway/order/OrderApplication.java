ad8f437fa9887c7f0819b30f5178bd089681de24
Even Holthe <even.holthe@bekk.no>
2022-02-24 11:54:50 +0100

    initial commit

---
+++ order/src/main/java/no/bekk/bekkaway/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package no.bekk.bekkaway.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
